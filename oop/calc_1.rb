5f7d094d52ed48c29b41a9dbbc19915edf1b7e73
Steen Clarke <cmclarkey@gmail.com>
2016-08-08 10:22:18 -0700

    Finish calc_1 exercise

--- oop/calc_1.rb
+++ oop/calc_1.rb
@@ -32,23 +32,7 @@ class SimpleCalculator
 
 end
 
-class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+class FancyCalculator < SimpleCalculator
 
   def square_root(number)
     Math.sqrt(number)
@@ -58,3 +42,14 @@ end
 
 # Write your own driver code below:
 
+my_calc = FancyCalculator.new
+
+puts "1 plus 4 is #{my_calc.add(1, 4)}"
+
+puts "6 minus 2 is #{my_calc.subtract(6, 2)}"
+
+puts "3 times 9 is #{my_calc.multiply(3, 9)}"
+
+puts "12 divided by 4 is #{my_calc.divide(12, 4)}"
+
+puts "The square root of 81 is #{my_calc.square_root(81)}"
