649f5ff64941c99ba4387c1917ef60e394784512
Steen Clarke <cmclarkey@gmail.com>
2016-08-08 10:33:19 -0700

    Finish calc_3 exercise

--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -24,23 +24,7 @@ class SimpleCalculator
 
 end
 
-class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+class FancyCalculator < SimpleCalculator
 
   def square_root(number)
     Math.sqrt(number)
@@ -48,27 +32,7 @@ class FancyCalculator
 
 end
 
-class WhizBangCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
-  def square_root(number)
-    Math.sqrt(number)
-  end
+class WhizBangCalculator < FancyCalculator
 
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
@@ -84,3 +48,18 @@ end
 
 # Copy your driver code from the previous exercise and more below:
 
+my_calc = WhizBangCalculator.new
+
+puts "1 plus 4 is #{my_calc.add(1, 4)}"
+
+puts "6 minus 2 is #{my_calc.subtract(6, 2)}"
+
+puts "3 times 9 is #{my_calc.multiply(3, 9)}"
+
+puts "12 divided by 4 is #{my_calc.divide(12, 4)}"
+
+puts "The square root of 81 is #{my_calc.square_root(81)}"
+
+puts "The hypotenuse bounded by two sides sizes 10 and 15 is #{my_calc.hypotenuse(10, 15)}"
+
+puts "3 to the power of 5 is #{my_calc.exponent(3, 5)}"
